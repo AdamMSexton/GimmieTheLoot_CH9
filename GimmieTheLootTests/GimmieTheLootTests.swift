23c4f50fddaf46f2e05052bace7291e05a1c8d42
Adam Sexton <asexton1@jacksonville.edu>
2025-10-07 15:34:43 -0400

    Initial Commit

---
+++ GimmieTheLootTests/GimmieTheLootTests.swift
@@ -0,0 +1,17 @@
+//
+//  GimmieTheLootTests.swift
+//  GimmieTheLootTests
+//
+//  Created by Apple Sucks on 10/7/25.
+//
+
+import Testing
+@testable import GimmieTheLoot
+
+struct GimmieTheLootTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
