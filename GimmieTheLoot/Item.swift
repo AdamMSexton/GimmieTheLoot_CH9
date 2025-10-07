561929903761a5cff54dc0079f4214ce1063545d
Adam Sexton <asexton1@jacksonville.edu>
2025-10-07 15:44:53 -0400

    Initial Setup

---
+++ GimmieTheLoot/Item.swift
@@ -0,0 +1,8 @@
+import UIKit
+
+class Item {
+    var name: String
+    var valueInDollars: Int
+    var serialNumber: String?
+    let dateCreated: Date
+}
---
+++ GimmieTheLoot/ItemsViewController.swift
@@ -0,0 +1,8 @@
+//
+//  ItemsViewController.swift
+//  GimmieTheLoot
+//
+//  Created by Apple Sucks on 10/7/25.
+//
+
+import Foundation
